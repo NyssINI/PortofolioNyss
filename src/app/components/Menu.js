19a6b6c3866ef13699a1134c3960766b78012cff
NyssINI <igustingurahdenysyudiantara@gmail.com>
2025-01-11 23:33:28 +0800

    update 3

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -18,57 +18,66 @@ const Menu = () => {
   };
 
   return (
-    <div className="menu-container relative h-screen">
-      <div className="relative z-10 menu-bar flex justify-between items-center p-4">
-        <div className="menu-logo text-white font-bold text-xl">
+    <div className="menu-container" ref={container}>
+      {/* Menu Bar */}
+      <div className="menu-bar">
+        <div className="menu-logo">
           <Link href="/">Codegrid</Link>
         </div>
-        <div
-          className="menu-open cursor-pointer text-white"
-          onClick={toggleMenu}
-        >
+        <div className="menu-open" onClick={toggleMenu}>
           <p>Menu</p>
         </div>
       </div>
 
+      {/* Menu Overlay */}
       {isMenuOpen && (
-        <div className="menu-overlay z-20">
-          <div className="menu-overlay-bar flex justify-between items-center w-full px-8 py-4 text-white">
+        <div className="menu-overlay">
+          {/* Overlay Bar */}
+          <div className="menu-overlay-bar">
             <div className="menu-logo">
               <Link href="/">Codegrid</Link>
             </div>
+            {/* Close Icon */}
             <div
-              className="menu-close cursor-pointer"
-              onClick={toggleMenu}
+              className="menu-close-icon"
+              onClick={toggleMenu} // Tambahkan handler di sini
             >
-              <p>Close</p>
+              <p>&#x2715;</p> {/* Simbol 'X' untuk close */}
             </div>
           </div>
-          <div className="menu-links space-y-6 mt-12 text-center">
-            {menuLink.map((link, index) => (
-              <div
-                className="menu-link-item text-white text-lg font-medium"
-                key={index}
-              >
-                <Link href={link.path} onClick={toggleMenu}>
-                  {link.label}
-                </Link>
+
+          {/* Menu Links */}
+          <div className="menu-copy">
+            <div className="menu-links">
+              {menuLink.map((link, index) => (
+                <div className="menu-link-item" key={index}>
+                  <div
+                    className="menu-link-item-holder"
+                    onClick={toggleMenu} // Menutup menu saat link diklik
+                  >
+                    <Link href={link.path} className="menu-link">
+                      {link.label}
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            {/* Info Section */}
+            <div className="menu-info">
+              <div className="menu-info-col">
+                <p>Nyssnigeng@gmail.com</p>
+                <p>087627681568</p>
               </div>
-            ))}
+              <div className="menu-link-info-col">
+                <a href="#">Instagram &#8599;</a>
+                <a href="#">X &#8599;</a>
+              </div>
+            </div>
           </div>
-          <div className="menu-info mt-8 text-center text-gray-300">
-            <p>Nyssnigeng@gmail.com</p>
-            <p>087627681568</p>
-          </div>
-          <div className="menu-social-links flex space-x-4 mt-6 text-gray-300">
-            <a href="#" className="hover:text-white">
-              Instagram &#8599;
-            </a>
-            <a href="#" className="hover:text-white">
-              X &#8599;
-            </a>
-          </div>
-          <div className="menu-preview mt-12 text-gray-300 cursor-pointer">
+
+          {/* Menu Preview */}
+          <div className="menu-preview">
             <p>View Showreel</p>
           </div>
         </div>
